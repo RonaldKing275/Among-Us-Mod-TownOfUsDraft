678c0fe00b7e775e9a8e991d5bc3239c3be74f21
RonaldKing275 <ronaldking0429@gmail.com>
2025-12-27 19:25:50 +0100

    zmianav2-5

--- DraftPlugin.cs
+++ DraftPlugin.cs
@@ -2,10 +2,14 @@ using BepInEx;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using Il2CppInterop.Runtime.Injection;
+using UnityEngine;
+using TownOfUsDraft; // Upewnij się, że namespace się zgadza
 
 namespace TownOfUsDraft
 {
     [BepInPlugin("TownOfUsDraft", "Town Of Us Draft Mode", "1.0.0")]
+    // Ważne: Informujemy BepInEx, że potrzebujemy TownOfUs
+    [BepInDependency("au.avengers.townofusmira", BepInDependency.DependencyFlags.HardDependency)] 
     public class DraftPlugin : BasePlugin
     {
         public static DraftPlugin Instance;
@@ -15,13 +19,23 @@ namespace TownOfUsDraft
         {
             Instance = this;
             
-            // Rejestracja klasy HUD (wymagane dla IL2CPP)
+            // 1. Rejestracja klas w systemie IL2CPP (WYMAGANE dla MonoBehaviour)
+            ClassInjector.RegisterTypeInIl2Cpp<DraftManager>();
             ClassInjector.RegisterTypeInIl2Cpp<DraftHud>();
             
-            // Aplikowanie patchy (w tym nowego HudPatch)
+            // 2. Aplikowanie patchy
             Harmony.PatchAll();
             
-            Log.LogInfo("Draft Mode Loaded & Patched.");
+            // 3. Dodanie Managera do sceny (Kluczowy krok!)
+            // Tworzymy pusty obiekt w grze, który będzie trzymał nasze skrypty
+            var go = new GameObject("TownOfUsDraftManager");
+            Object.DontDestroyOnLoad(go); // Żeby nie znikał przy zmianie sceny
+            
+            // Dodajemy komponenty - to uruchomi ich metody Awake() i ustawi Instance
+            go.AddComponent<DraftManager>();
+            go.AddComponent<DraftHud>();
+            
+            Log.LogInfo("Draft Mode Loaded, Registered & Patched.");
         }
     }
 }
